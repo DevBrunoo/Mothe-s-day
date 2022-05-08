836e588c850d6fb311c954df69bbbf96e002aa2a
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-05-08 19:41:11 -0300

    day

---
+++ server.js
@@ -0,0 +1,15 @@
+const { resolveSoa } = require('dns');
+const express = require('express')
+const app = express()
+const PORT = 3000;
+
+
+app.set('view engine', 'ejs')
+app.get('', (req, res) =>{
+    res.render('layout.ejs')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Rodando na porta ${PORT}`)
+})
